353b5f8aea223861b5e9c4fec9adc5f1f8c8766b
pshingsl <w455ok@gmail.com>
2025-08-02 03:25:25 +0900

    자바 논리형 실습

--- ch02/BooleanExample.java
+++ ch02/BooleanExample.java
@@ -2,9 +2,17 @@ package study.ch02;
 
 public class BooleanExample {
     public static void main(String[] arg) {
-        float v1 = 3.14f;
-        double v2 = 4.124;
-        System.out.println(v1);      // 3.14
-        System.out.println(v2);      // 4.124
+        boolean finish = true;
+        if (finish) {
+            System.out.println("끝났습니다.");
+        } else {
+            System.out.println("아직 안 끝났습니다.");
+        }
+
+        int score = 80;
+        boolean a = score < 70;
+        boolean b = score >= 80;
+        System.out.println("result1: " + a);
+        System.out.println("result2: " + b);
     }
 }
