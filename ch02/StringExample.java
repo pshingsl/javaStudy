1f8e62043da728255274567cb5917334c5a999bf
pshingsl <w455ok@gmail.com>
2025-08-05 01:58:35 +0900

    자바 참조 타입 문자열 연습

---
+++ ch02/StringExample.java
@@ -0,0 +1,11 @@
+package study.ch02;
+
+public class StringExample {
+    public static void main(String[] arg) {
+    String name = "박상혁";
+    String height = "82.3";
+    String belt = "블루";
+
+    System.out.println("이름은 " + name + "이고 " + belt +"이며 몸무게는 "+ height+"kg입니다");
+    }
+}
