1ab5ecf6d4162b78d47cdae440714b7aa05734ff
pshingsl <w455ok@gmail.com>
2025-08-08 02:26:59 +0900

    if문 실습

---
+++ ch04/Ex4_1.java
@@ -0,0 +1,10 @@
+package study.ch04;
+
+public class Ex4_1 {
+    public static void main(String[] args){
+        int age = 20;
+        if (age > 10) {
+            System.out.println("10보다 큽니다.");
+        }
+    }
+}
