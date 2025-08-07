2823969afeeb13547970800762e1b4ca979022ce
pshingsl <w455ok@gmail.com>
2025-08-08 02:27:38 +0900

    if문 else문 실습

---
+++ ch04/Ex4_2.java
@@ -0,0 +1,18 @@
+package study.ch04;
+
+import java.util.Scanner;
+
+public class Ex4_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("숫자를 입력해주세요.");
+
+        int num = sc.nextInt();
+
+        if (num > 10) {
+            System.out.println("10보다 큽니다..");
+        } else {
+            System.out.println("10보다 작습니다.");
+        }
+    }
+}
