5e450dc0d477e553d4fe8be86aae97133ef2cf64
pshingsl <w455ok@gmail.com>
2025-08-08 02:27:59 +0900

    if문 else if문 실습

---
+++ ch04/Ex4_3.java
@@ -0,0 +1,20 @@
+package study.ch04;
+
+import java.util.Scanner;
+
+public class Ex4_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("숫자를 입력해주세요.");
+
+        int num = sc.nextInt();
+
+        if (num > 10) {
+            System.out.println("10보다 큽니다..");
+        } else if(num == 10){
+            System.out.println("10 입니다.");
+        } else {
+            System.out.println("10 작습니다.");
+        }
+    }
+}
