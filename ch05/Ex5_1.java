1a79c3426d502fa48595cd0e9225a3329f4a16b1
pshingsl <w455ok@gmail.com>
2025-08-09 03:35:15 +0900

    배열 선언 및 초기화

---
+++ ch05/Ex5_1.java
@@ -0,0 +1,18 @@
+package study.ch05;
+
+public class Ex5_1 {
+    public static void main(String[] args) {
+        //int[] score; // 배열 생성(참조변수)
+        //score = new int[5];
+        // [] 생략하면 Array initializer is not allowed here 에러 발생
+
+        int[] score = new int[5];
+        score[3] = 100;
+        System.out.println("score[0]" + score[0]);
+        System.out.println("score[1]" + score[1]);
+        System.out.println("score[2]" + score[2]);
+        System.out.println("score[3]" + score[3]);
+        System.out.println("score[4]" + score[4]);
+
+    }
+}
