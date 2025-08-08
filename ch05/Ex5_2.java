86b84ae74aa533588a8aa5a3dad35f37f6f47d7b
pshingsl <w455ok@gmail.com>
2025-08-09 03:35:39 +0900

    반복문으로 출력한 배열

---
+++ ch05/Ex5_2.java
@@ -0,0 +1,16 @@
+package study.ch05;
+
+public class Ex5_2 {
+    public static void main(String[] args) {
+        System.out.println("== 배열의 길이 ==");
+        int arr[] = new int[6]; // 길이가 6인 배열 생성
+        System.out.println(arr.length);
+
+        int[] arr2 = new int[6];
+        for (int i = 0; i < arr2.length; i++) {
+            // 배열을 총 개수를 담을 변수 선언
+            arr2[i] = i * 10 + 10;
+            System.out.println(arr2[i]);
+        }
+    }
+}
