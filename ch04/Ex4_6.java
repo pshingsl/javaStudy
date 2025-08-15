5f95e75c65df74fddfb72a6a84a6a1fd7a063ffa
pshingsl <w455ok@gmail.com>
2025-08-16 04:00:05 +0900

    다중for

--- ch04/Ex4_6.java
+++ ch04/Ex4_6.java
@@ -2,6 +2,7 @@ package study.ch04;
 
 public class Ex4_6 {
     public static void main(String[] args) {
+        // 다중 for
          for(int i=2; i<10; i++){
              for(int j=1; j<10; j++){
                  System.out.println("ixj="+(i*j));
