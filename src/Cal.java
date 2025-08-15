ac86da806ec76ed99437287c05006f7f1f627265
pshingsl <w455ok@gmail.com>
2025-08-16 03:28:51 +0900

    상속 Scanner

--- src/Cal.java
+++ src/Cal.java
@@ -1,4 +1,60 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Cal {
+    public static void main(String[] args) {
+        // 입력을 받는데 문자열로 받아옴
+        Scanner sc = new Scanner(System.in);
+
+        Calc c = new Calc();
+        while (true) {
+            System.out.println("두 숫자를 입력해주세요.");
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            char opt = sc.next().charAt(0); // 입력 받아야함 next() 문자열을 반환
+            // charAt는 문자로 추출하는 과정 우리는 기호만 받아올거니까 이걸 사용함
+            switch (opt) {
+                case '+':
+                    System.out.println(c.add(a, b));
+                    break;
+                case '-':
+                    System.out.println(c.minus(a, b));
+                    break;
+                case '*':
+                    System.out.println(c.multi(a, b));
+                    break;
+                case '/':
+                    System.out.println(c.divide(a, b));
+                    break;
+                default:
+                    System.out.println("잘못된 기호 입니다. +, - ,*, / 중 골라주세요!");
+            }
+        }
+    }
+}
+
+class Calc {
+    int a;
+    int b;
+
+    //  this로 초기화하면 메서드와 클래스의 매개변수가 달라서 리턴할때 0으로 됨
+    
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int minus(int a, int b) {
+        return a - b;
+    }
+
+    public int multi(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (a == 0 && b == 0) {
+            System.out.println("0을 대입해서 나눌 수 없습니다.");
+        }
+        return (double) a / b;
+    }
 }
--- src/Test3.java
+++ src/Test3.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Test3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.print("배열의 크기를 입력하세요. : ");
+        try {
+            int a = sc.nextInt();
+
+            if (a <= 0)
+                throw new IllegalArgumentException();
+
+            int[] arr = new int[a];
+            System.out.println(a + " 개의 정수를 입력해주세요: ");
+
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print("정수 " + (i + 1) + ": ");
+                arr[i] = sc.nextInt();
+                list.add(arr[i]);
+            }
+            for (int i = 0; i < list.size(); i++) {
+                for (int j = i + 1; j < list.size(); j++) {
+                    if (list.get(i).equals(list.get(j))) { // 이 부분이 조건문
+                        System.out.println("중복 발견: " + list.get(i));
+                    }
+                }
+            }
+    } catch(
+    InputMismatchException e)
+
+    {
+        System.out.println("숫자로 입력해주세요. " + e.getMessage());
+    }
+}
 }
