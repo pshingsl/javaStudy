866a6b992c9fdb6c0fc231c07a0ac1016d88f0b2
pshingsl <w455ok@gmail.com>
2025-08-16 03:59:36 +0900

    if-else

--- ch04/Ex4_2.java
+++ ch04/Ex4_2.java
@@ -3,6 +3,7 @@ package study.ch04;
 import java.util.Scanner;
 
 public class Ex4_2 {
+    // if else
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("숫자를 입력해주세요.");
