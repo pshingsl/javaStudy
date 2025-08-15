6f891d5cb5f9ab7e82dbcef9d5a3b238a6a767ff
pshingsl <w455ok@gmail.com>
2025-08-16 04:00:13 +0900

    while

--- ch04/Ex4_7.java
+++ ch04/Ex4_7.java
@@ -2,6 +2,7 @@ package study.ch04;
 
 public class Ex4_7 {
     public static void main(String[] args) {
+        // while
         int i = 1;
         while (i <= 10) {
             System.out.println(i);
