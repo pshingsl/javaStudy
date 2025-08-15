a5e73e1da3fd59a3fc7fd4bd9ef4ec68c5f2f748
pshingsl <w455ok@gmail.com>
2025-08-16 03:10:53 +0900

    instaceOf 연산자

--- ch07/Ex7_11.java
+++ ch07/Ex7_11.java
@@ -4,7 +4,7 @@ public class Ex7_11 {
     public static void main(String[] args) {
         FireEngine2 f2 = new FireEngine2();
 
-        // instaceOf 연산자
+        // instaceOf 연산자 실습
         if (f2 instanceof FireEngine2) {
             System.out.println("f2는 FireEngine의 인스턴스입니다.");
         }
