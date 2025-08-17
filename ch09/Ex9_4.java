9c0959518d6b27e5eb1956095e873bae82ef645e
pshingsl <w455ok@gmail.com>
2025-08-18 01:20:39 +0900

    String 생성자 메소드 실습

--- ch09/Ex9_4.java
+++ ch09/Ex9_4.java
@@ -19,31 +19,38 @@ public class Ex9_4 {
         char c1 = s.charAt(1); // Hello -> e를 반환
 
         // 문자열을 사전순서로 비교 같으면 0 사전순 이전 음수 사전순 이후 양수
+        // 비교 compareTo()
         int i = "aaa".compareTo("aaa"); // 0
         int i2 = "aaa".compareTo("bbb"); // -1
         int i3 = "bbb".compareTo("aaa"); // 1
 
         // 문자열 결합
+        // concat()
         String con = s.concat(" World"); // Hello World
 
         // 문자열 포함 검사 반환: true or false
+        // contains()
         String s4 = "world";
         boolean b = s4.contains("rl"); // true
 
         // 문자열로 끝났는지 검사 반환: true or false
+        // endWith()
         boolean b2 = s4.endsWith("d"); // true
 
         // 문자열 비교 반환: true or false
+        // equals()
         String s5 = "java";
         boolean b3 = s5.equals("java"); // true
         boolean b4 = s5.equals("Java"); // false
 
         //  문자열 비교(대소문자 구분x) 반환: true or false
+        // equalsIgnoreCase()
         String s6 = "java";
         boolean b5 = s5.equalsIgnoreCase("java"); // true
         boolean b6 = s5.equalsIgnoreCase("Java"); // false
 
         // 문자 존재확인 반환: 해당 위치 알려줌
+        // indexOf()
         String s7 = "Hello";
         int idx1 = s7.indexOf('l'); // 2 똑같은게 있으면 0번째부터 시작해서 가까운걸 반환
         int idx2 = s7.indexOf('H'); // 0
@@ -54,11 +61,13 @@ public class Ex9_4 {
         int idx4 = s8.indexOf('e', 2); // -1
 
         // 지정 문자 존재확인 맨뒤에서 부터 탐색
+        // lastIndexOf()
         String s9 = "java.lang.object";
         int id = s9.lastIndexOf('.');   // 10 맨뒤에서 부터 탐색
         int id2 = s9.indexOf('.');          // . 맨앞에서 부터 탐색
 
         // 문자 분리
+        // split()
         String ani = "dog, cat, bear";
         String[] arr = ani.split(","); // arr[0] = "dog" arr[1] = "cat" arr[2] = "bear"
 
@@ -67,24 +76,29 @@ public class Ex9_4 {
         String[] arr2 = ani.split(",", 2); // arr[0] = "dog" arr[1] = "cat, bear"
 
         // 주어진 문자열로 시작하는지 검사 반환: true or false
+        // startWith()
         String q1 = "java.lang.object";
         boolean b7 = q1.startsWith("java"); // true -> 맨 앞에서부터 탐색
         boolean b8 = q1.startsWith("lang"); // false
 
         // 시작~끝 문자열 포함 여부
+        // subString()
         String q2 = "java.lang.object";
         String c2 = q2.substring(10);  // object 10번째 부터 시작
         String c3 = q2.substring(5, 9);          //  lang 5번째부터 9-1까지 출력
 
         // 모든 문자열 소문자 변환
+        // toLowerCase()
         String q3 = "Hello";
         String t1 = q3.toLowerCase(); // hello
 
         // 모든 문자열 대문자 변환
+        // toUpperCase()
         String q4 = "Hello";
         String t2 = q4.toUpperCase(); // HELLO
 
         // 문자 공백 제거 -> 중간에 있는 공백은 제거 안됨
+        // trim()
         String q5 = "Hello World";
         String t3 = q5.trim();  // Hello World
 
@@ -92,6 +106,7 @@ public class Ex9_4 {
         String t4 = q6.trim(); // HelloWorld
 
         // 지정된 값 문자열로 변환
+        // valueOf()
         String q7 = String.valueOf(10000); // "10000"
     }
 }
