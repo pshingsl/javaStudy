a5d3db0a942c7219d8693a8018d7f955ace8da02
pshingsl <w455ok@gmail.com>
2025-08-23 23:17:51 +0900

    자바 연습

---
+++ src/practice/T1.java
@@ -0,0 +1,14 @@
+package practice;
+
+import java.util.Scanner;
+
+public class T1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 입력 정수 1개를 입력
+        // 출력: 정수 출력 15
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+}
