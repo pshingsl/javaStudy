6ae4533aa549d749651e1e71ae2b47b204f10a52
pshingsl <w455ok@gmail.com>
2025-08-23 23:32:33 +0900

    prac1

--- src/practice/Test1.java
+++ src/practice/Test1.java
@@ -7,8 +7,9 @@ public class Test1 {
         Scanner sc = new Scanner(System.in);
 
         // 입력 정수 1개를 입력
-        // 출력: 정수 출력 15
+        // 출력: 정수 출력
         int a = sc.nextInt();
         System.out.println(a);
+        //15
     }
 }
