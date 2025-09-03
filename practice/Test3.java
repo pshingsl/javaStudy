c48a8d5b0dbab40df8781d2f73ceada604515104
pshingsl <w455ok@gmail.com>
2025-09-04 03:27:23 +0900

    문자 두개 위치 바꿔 출력

--- practice/Test3.java
+++ practice/Test3.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Test3 {
     public static void main(String[] args) {
+        // 문자 두개 위치 바꿔 출력
         Scanner sc = new Scanner(System.in);
 
         String a = sc.next();
