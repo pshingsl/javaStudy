a397a60a4003a466d329e811de4f807ef2af4273
pshingsl <w455ok@gmail.com>
2025-09-04 03:17:44 +0900

    문자 2개 입력받아 순서 바꿔 출력하기

---
+++ practice/Test1.java
@@ -0,0 +1,12 @@
+package javaStudy.practice;
+
+import java.util.Scanner;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+}
---
+++ practice/Test2.java
@@ -0,0 +1,13 @@
+package javaStudy.practice;
+
+import java.util.Scanner;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String  b = sc.next();
+        System.out.println(b +" " + a);
+    }
+}
