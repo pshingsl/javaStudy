22dc6e4a1097cd3143bb28c2f2fe048faa8833fc
pshingsl <w455ok@gmail.com>
2025-09-04 03:27:41 +0900

    정수 2개 출력

--- practice/Test2.java
+++ practice/Test2.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Test2 {
     public static void main(String[] args) {
+        // 정수 2개 출력
         Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
